ff6bb012d6133408a6d094a98c79ede088121cd0
Coder731 <stephenobrien1@gmail.com>
2023-12-27 16:54:09 +0000

    Add comments from tutorial

--- main.cpp
+++ main.cpp
@@ -7,9 +7,9 @@ int main()
 	int firstvalue, secondvalue;
 	int* mypointer;
 
-	mypointer = &firstvalue;
-	*mypointer = 10;
-	mypointer = &secondvalue;
+	mypointer = &firstvalue; // mypointer assigned address of firstvalue using address-of operator &
+	*mypointer = 10; // value pointed to by mypointer is assigned value of 10
+	mypointer = &secondvalue; // pointer may point to different variables: repeat previous 2 line process
 	*mypointer = 20;
 	cout << "firstvalue is " << firstvalue << '\n';
 	cout << "secondvalue is " << secondvalue << '\n';
