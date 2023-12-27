c67570e8741385764337d9b707c9994ce3e6739b
Coder731 <stephenobrien1@gmail.com>
2023-12-27 13:09:48 +0000

    Add arrays example

--- main.cpp
+++ main.cpp
@@ -1,15 +1,16 @@
-// custom countdown using while
+// arrays example
 #include <iostream>
 using namespace std;
 
+int foo[] = { 16, 2, 77, 40, 12071 };
+int n, result = 0;
+
 int main()
 {
-    int n = 10;
-
-    while (n > 0) {
-        cout << n << ", ";
-        --n;
+    for (n = 0; n < 5; ++n)
+    {
+        result += foo[n];
     }
-
-    cout << "liftoff!\n";
+    cout << result;
+    return 0;
 }
