1a2b9935a20d68d5cbce71f4edb40dd7b6224d11
Coder731 <stephenobrien1@gmail.com>
2023-12-27 19:16:24 +0000

    Add pointers with arrays example

--- main.cpp
+++ main.cpp
@@ -2,29 +2,16 @@
 #include <iostream>
 using namespace std;
 
-// Notice that there are expressions with pointers p1 and p2, 
-// both with and without the dereference operator (*). 
-// The meaning of an expression using the dereference operator (*) 
-// is very different from one that does not. 
-// When this operator precedes the pointer name, 
-// the expression refers to the value being pointed, 
-// while when a pointer name appears without this operator, 
-// it refers to the value of the pointer itself 
-// (i.e., the address of what the pointer is pointing to).
-
 int main()
 {
-	int firstvalue = 5, secondvalue = 15;
-	int * p1, * p2; // asterisk (*) for each pointer
-
-	p1 = &firstvalue;  // p1 = address of firstvalue
-	p2 = &secondvalue; // p2 = address of secondvalue
-	*p1 = 10;          // value pointed to by p1 = 10
-	*p2 = *p1;         // value pointed to by p2 = value pointed to by p1
-	p1 = p2;           // p1 = p2 (value of pointer is copied)
-	*p1 = 20;          // value pointed to by p1 = 20
-
-	cout << "firstvalue is " << firstvalue << '\n'; // firstvalue is 10
-	cout << "secondvalue is " << secondvalue << '\n'; // secondvalue is 20
-	return 0;
+    int numbers[5];
+    int* p;
+    p = numbers;  *p = 10;
+    p++;  *p = 20;
+    p = &numbers[2];  *p = 30;
+    p = numbers + 3;  *p = 40;
+    p = numbers;  *(p + 4) = 50;
+    for (int n = 0; n < 5; n++)
+        cout << numbers[n] << ", ";
+    return 0;
 }
