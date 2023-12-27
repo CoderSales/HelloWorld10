13fb951f48cbee4fd0aa274412a92c2d690b2aca
Coder731 <stephenobrien1@gmail.com>
2023-12-27 19:17:35 +0000

    Add output as comment

--- main.cpp
+++ main.cpp
@@ -12,6 +12,6 @@ int main()
     p = numbers + 3;  *p = 40;
     p = numbers;  *(p + 4) = 50;
     for (int n = 0; n < 5; n++)
-        cout << numbers[n] << ", ";
+        cout << numbers[n] << ", "; // 10, 20, 30, 40, 50,
     return 0;
 }
